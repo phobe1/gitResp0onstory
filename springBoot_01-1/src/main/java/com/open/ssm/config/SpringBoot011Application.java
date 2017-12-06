c015df5f078787a6deaf738cf402227d5ffe2e51
phobe1 <573388810@qq.com>
2017-12-06 17:50:23 +0800

    eee

---
+++ springBoot_01-1/src/main/java/com/open/ssm/config/SpringBoot011Application.java
@@ -0,0 +1,12 @@
+package com.open.ssm.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot011Application.class, args);
+	}
+}

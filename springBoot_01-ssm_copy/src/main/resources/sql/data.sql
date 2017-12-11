05c1c9f311c514ab60f4657124e1e320b1b682a1
phobe1 <573388810@qq.com>
2017-12-11 12:21:47 +0800

    hgg
    766

---
+++ springBoot_01-ssm_copy/src/main/resources/sql/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `blog_user` VALUES
+('1', 'admin', '233', '2ff4d63f4adb19d17cc0a99d2532b6a1', 'zdshen@163.com', null, null, null, null, '2', null),
+('2', 'sysadmin', '系统管理员', '9bc34549d565d9505b287de0cd20ac77be1d3f2c', null, null, null, null, null, '2', null),
+('3', 'tom', '汤姆', '123456', '123@163.com', '北京市', null, null, '2017-12-10 12:04:52', null, null);
+COMMIT;
